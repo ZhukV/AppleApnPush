59db07a3c0759f755c2fc4bb0765bc256b0c8262
Vitaliy Zhuk <zhuk2205@gmail.com>
2013-02-21 08:41:59 +0000

    Fix typo in word copyring

--- src/Apple/ApnPush/Connection/AbstractConnection.php
+++ src/Apple/ApnPush/Connection/AbstractConnection.php
@@ -5,7 +5,7 @@
  *
  * (c) Vitaliy Zhuk <zhuk2205@gmail.com>
  *
- * For the full copyring and license information, please view the LICENSE
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code
  */
 
