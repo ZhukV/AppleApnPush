b669c5d7aa27af29e736c0b57c05bd33a3b2a6d5
Vitaliy Zhuk <zhuk2205@gmail.com>
2016-12-26 16:19:42 +0200

    Add PHP version for build:

--- src/Model/Payload.php
+++ src/Model/Payload.php
@@ -11,8 +11,6 @@
 
 namespace Apple\ApnPush\Model;
 
-use \JsonSerializable;
-
 /**
  * Payload model
  */
@@ -78,7 +76,7 @@ class Payload
      */
     public function withCustomData(string $name, $value)
     {
-        if ($value && !is_array($value) && !is_scalar($value) && !$value instanceof JsonSerializable) {
+        if ($value && !is_array($value) && !is_scalar($value) && !$value instanceof \JsonSerializable) {
             throw new \InvalidArgumentException(sprintf(
                 'The custom data value should be a scalar or \JsonSerializable instance, but "%s" given.',
                 is_object($value) ? get_class($value) : gettype($value)
